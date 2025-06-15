ef02a061c9a362c89c5611877fbd372b4e60a21f
f-ferraro <58588827+f-ferraro@users.noreply.github.com>
2025-06-15 10:23:30 -0700

    Update R_preprocess_modkitoutput.R

--- R_preprocess_modkitoutput.R
+++ R_preprocess_modkitoutput.R
@@ -1,38 +1,34 @@
-sampleID=""
+#!/usr/bin/env Rscript
 
-#######################################################################################################################
-#######################################################################################################################
-##################################################### No filters ######################################################
-#######################################################################################################################
-#######################################################################################################################
-temp = read.delim(paste0(sampleID,".pseudoepic.bed.cpgID"), header=FALSE)
- 
-temp$V11 = temp$V11 / 100
+args <- commandArgs(trailingOnly = TRUE)
 
-output = data.frame("IllmnID" = temp$V19, 
-                    'Sample' = temp$V11)
- 
-names(output)[names(output) == "Sample"] <- sampleID
+if (length(args) != 3) {
+  stop("Usage: Rscript R_preprocess_modkitoutput.R <sampleID> <input_path> <output_path>")
+}
 
-write.table(output, paste0(sampleID,"_EPIC_stranded_unfiltered.tsv"),
-            quote = F, sep = "\t", row.names = F, )
- 
-#######################################################################################################################
-#######################################################################################################################
-##################################################### With filters ####################################################
-#######################################################################################################################
-#######################################################################################################################
-temp$TotalDepth = rowSums(temp[, colnames(temp) %in% paste0("V", seq(12,18,1))])
-temp$UsableFraction = temp$V5 / temp$TotalDepth
- 
-temp = temp[temp$UsableFraction > 0.5,]
-temp = temp[(temp$V5 > 8 & temp$V5 < 50),]
+sampleID    <- args[1]
+input_path  <- args[2]
+output_path <- args[3]
 
-output = data.frame("IllmnID" = temp$V19, 
-                    'Sample' = temp$V11)
- 
-names(output)[names(output) == "Sample"] <- sampleID
+# Read input file
+temp <- read.delim(input_path, header = FALSE)
 
-write.table(output, paste0(sampleID,"_EPIC_stranded_filtered.tsv"),
-            quote = F, sep = "\t", row.names = F, )
- 
+# Normalize V11 to floats
+temp$V11 <- temp$V11 / 100
+
+# QC metrics
+temp$TotalDepth <- rowSums(temp[, paste0("V", 12:18)])
+temp$UsableFraction <- temp$V5 / temp$TotalDepth
+
+temp_filtered <- temp[temp$UsableFraction > 0.5, ]
+temp_filtered <- temp_filtered[temp_filtered$V5 > 8 & temp_filtered$V5 < 50, ]
+
+# Create filtered output
+output_filtered <- data.frame(IllmnID = temp_filtered$V19, Sample = temp_filtered$V11)
+names(output_filtered)[names(output_filtered) == "Sample"] <- sampleID
+
+write.table(
+  output_filtered,
+  output_path,
+  quote = FALSE, sep = "\t", row.names = FALSE
+)
